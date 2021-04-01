5dad290004da829e0f26ac1ef01b2fc96886a000
Scott Byrne <scott.byrne@forestry.io>
2021-04-01 19:44:32 -0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
